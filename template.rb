4c20976faa2d6dd2846e30ca46dff8db7e615604
u16suzu <u16suzu@gmail.com>
2012-04-15 21:51:47 +0900

    Fix: ruby style template pattern.

--- template.rb
+++ template.rb
@@ -1,10 +1,11 @@
 # template pattern 親が流れをきめる。
 # 具体的な処理は子で定義する。
+# rubyではこんな感じ親で子が定義するメソッドを定義しなくてよい。
 
 class Parent 
-  def method1 ; p "not dev"  end 
-  def method2 ; p "not dev"  end 
-  def method3 ; p "not dev"  end 
+  # def method1 ; p "not dev"  end 
+  # def method2 ; p "not dev"  end 
+  # def method3 ; p "not dev"  end 
 
   def act
     method1 
@@ -13,7 +14,7 @@ class Parent
   end
 end
 
-Parent.new.act
+#Parent.new.act
 
 class Child < Parent
   def method1 ; p "1st"  end 
