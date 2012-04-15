62ac13145c4ddff8e13f51185fe0424ae6adbbbf
u16suzu <u16suzu@gmail.com>
2012-04-15 21:45:23 +0900

    Add: template pattern

---
+++ template.rb
@@ -0,0 +1,25 @@
+# template pattern 親が流れをきめる。
+# 具体的な処理は子で定義する。
+
+class Parent 
+  def method1 ; p "not dev"  end 
+  def method2 ; p "not dev"  end 
+  def method3 ; p "not dev"  end 
+
+  def act
+    method1 
+    method2 
+    method3 
+  end
+end
+
+Parent.new.act
+
+class Child < Parent
+  def method1 ; p "1st"  end 
+  def method2 ; p "2nd"  end 
+  def method3 ; p "3rd"  end 
+  
+end
+
+Child.new.act
